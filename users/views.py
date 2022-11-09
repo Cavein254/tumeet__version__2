5a7ed458ba1b64d4066dbcce63268621b875ca87
johnmuthua <caveomuthama@gmail.com>
2022-11-09 12:31:07 +0300

    fix:Added github authentication

--- users/views.py
+++ users/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from allauth.socialaccount.providers.github import views as github_views
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
+from rest_auth.registration.views import SocialLoginView
 
-# Create your views here.
+
+class GithubLogin(SocialLoginView):
+    adapter_class = github_views.GitHubOAuth2Adapter
+    client_class = OAuth2Client
+
+    @property
+    def callback_url(self):
+        return self.request.build_absolute_uri('github_callback')
